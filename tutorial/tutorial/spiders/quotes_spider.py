bcf81337eab722ec5fd3428d3770f6542faf9ddf
noseparte <noseparte@aliyun.com>
2019-12-09 18:09:33 +0800

    /=: quotes_spider抓取英雄杀官网首页结构，做分析

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -6,8 +6,10 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'https://yxs.qq.com/webplat/info/news_version3/416/1620/1695/1696/1700/m1622/201312/241458.shtml',
+            'http://quotes.toscrape.com/page/2/http://quotes.toscrape.com/page/2/',
+            'https://yxs.qq.com/webplat/info/news_version3/416/1620/1695/1698/m1628/201203/56650.shtml?2',
+            'https://yxs.qq.com/webplat/info/news_version3/416/1620/1695/1699/m1628/201203/56651.shtml?3',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
