3f87e19fbf79acc2b84991e8042fc93a095fffa4
noseparte <noseparte@aliyun.com>
2020-01-14 18:27:25 +0800

    /=: 终于解决了网页编码为gb2312乱码问题
    body = response.body.decode(encoding)
    https://docs.scrapy.org/en/latest/topics/request-response.html

--- tutorial/tutorial/spiders/card_spider.py
+++ tutorial/tutorial/spiders/card_spider.py
@@ -1,12 +1,12 @@
+# encoding:UTF-8
+from bs4 import BeautifulSoup
 import scrapy
 from scrapy.selector import Selector
 import sys
 
+
 class CardSpider(scrapy.Spider):
     name = "card"
-    reload(sys)
-    # sys.setdefaultencoding('gb2312')
-    sys.setdefaultencoding('utf-8')
 
     def start_requests(self):
         urls = [
@@ -17,16 +17,58 @@ class CardSpider(scrapy.Spider):
 
     def parse(self, response):
         encoding = response.encoding
+        print("response encoding %s " % encoding)
+        default_encoding = sys.getdefaultencoding()
+        print("default_encoding %s " % default_encoding)
+        print("===============================================================================")
+        print("===============================================================================")
+
+        # soup = BeautifulSoup(response.body, fromEncoding=default_encoding)
+        # print("soup %s " % soup)
         # html = response.content.decode(encoding, "ignore")
-        body = response.body
+        body = response.body.decode(encoding)
         q = Selector(text=body)
+
+        # 武将属性区域
+        roll_img = q.css(".hero-intro #roll_img p img::attr(src)").extract_first()
+        print("武将牌的头像图片地址为： " + "http:" + roll_img)
+        blood = len(q.css(".hero-intro .blood .grain").extract())
+        print("武将牌的初始血量为：%s " % blood)
+        Identity = q.css(".hero-intro .intro h4 .don::text").get()
+        print("武将牌的身份为：%s " % Identity)
+        name = q.css(".hero-intro .intro h4 .fwb::text").extract_first()
+        print("武将牌的名称为：%s " % name)
+        skill = q.css(".hero-intro .intro .cb::text").extract_first()
+        print("武将牌的技能说明：%s " % skill)
+
+        # 技能介绍
+        skills = q.css(".skill").extract()
+        for skill_intro in skills:
+            intro = Selector(text=skill_intro)
+            skill_name = intro.css(".fwb::text").extract_first()
+            skill_type = intro.css(".ml10::text").extract_first()
+            skill_desc = intro.css("p::text").extract_first()
+            self.log('skill_name %s \n ' % skill_name)
+            self.log('skill_type %s \n ' % skill_type)
+            self.log('skill_desc %s \n ' % skill_desc)
+
+        # 技能规则
+        rules = q.css(".intro .c896 p::text").extract()
+        for rule in rules:
+            self.log('rule %s \n ' % rule)
+
+        # 人物生平
+        biography = q.css(".hero-intro .more-intro h5::text").extract_first()
+        self.log('biography %s \n ' % biography)
+        biography_intros = q.css(".hero-intro .more-intro p::text").extract()
+        for biography_intro in biography_intros:
+            self.log('biography_intro %s \n ' % biography_intro)
+
         items = q.css(".cont-box ul li a span::text").extract()
         filename = 'hero-kills-card.html'
-        with open(filename, 'ws') as f:
+        with open(filename, 'w') as f:
             for item in items:
                 # print(chardet.detect(item))
                 name = item.encode('utf-8')
                 self.log('result %s \n' % name)
                 f.write(name + '\n')
-
-
